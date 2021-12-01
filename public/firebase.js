dd085e790e5f8a0bd5cdcb129d18d8f4b3490a2d
Wayne Rocha <62760711+WayneRocha@users.noreply.github.com>
2021-12-01 16:54:17 -0300

    <built>

---
+++ public/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBB1lrlccqm87igFm6ZvD24Kt4Kth0EnrE",
+    authDomain: "agenda-clientes-4a42d.firebaseapp.com",
+    projectId: "agenda-clientes-4a42d",
+    storageBucket: "agenda-clientes-4a42d.appspot.com",
+    messagingSenderId: "540415155941",
+    appId: "1:540415155941:web:a34160831ff5bce02e3e0c"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- server/t.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-echo NULL ?: 52;
